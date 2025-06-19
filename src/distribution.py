98f6ff00cfe9a57fca5dc47b876192f8800a2e2b
ethaaalpha <geekofgamer@gmail.com>
2025-06-19 12:12:52 +0200

    finalize distribution script

--- src/distribution.py
+++ src/distribution.py
@@ -1,10 +1,25 @@
 import os
 import sys
-
+import matplotlib
+import matplotlib.pyplot as plt
 from preprocessing.loader import Loader
+
+def generate_histogram(data: dict[str, int]):
+    classes = list(data.keys())
+    counts = list(data.values())
+    colors = matplotlib.colors.TABLEAU_COLORS
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(classes, counts, color=colors, edgecolor='black')
+    plt.xlabel("Classes")
+    plt.ylabel("Number of Images")
+    plt.title("Image Count per Class")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     if (len(sys.argv) == 2 and os.path.exists(sys.argv[1])):
-        print(Loader(sys.argv[1]).count())
+        generate_histogram(Loader(sys.argv[1]).count())
     else:
         print("./Distribution.py <dataset_folder>")
