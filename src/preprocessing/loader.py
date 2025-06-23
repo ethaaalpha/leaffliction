d2a486aec253c2641888d66ab04d6a7d86f68b3c
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-06-23 14:56:22 +0200

    add direct_dir support loader

--- src/preprocessing/loader.py
+++ src/preprocessing/loader.py
@@ -1,8 +1,9 @@
 import os
 
 class Loader():
-    def __init__(self, path):
+    def __init__(self, path, direct_dir=False):
         self._path = path
+        self._direct_dir = direct_dir
 
     def parse(self) -> dict[str, list[str]]:
         if not (os.path.exists(self._path) and os.path.isdir(self._path)):
@@ -10,10 +11,15 @@ class Loader():
         if not (os.listdir(self._path)):
             raise ValueError("The path directory is empty!")
 
-        result = {c: None for c in sorted(os.listdir(self._path))}
-        for cat in result.keys():
-            cat_path = os.path.join(self._path, cat)
-            result[cat] = [os.path.join(cat_path, path) for path in sorted(os.listdir(cat_path))]
+        ldir = sorted(os.listdir(self._path))
+        result = {}
+
+        if self._direct_dir:
+            result["files"] = [os.path.join(self._path, file) for file in ldir]
+        else:
+            for cat in ldir:
+                cat_path = os.path.join(self._path, cat)
+                result[cat] = [os.path.join(cat_path, path) for path in sorted(os.listdir(cat_path))]
 
         return result
     
