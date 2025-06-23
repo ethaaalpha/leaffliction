64e5d33963749c9ba1416d1effbbb6e9e20d73bf
nmilan <milan.nicolas@rocketmail.com>
2025-06-24 01:59:28 +0200

    complete prediction and add window with images plot

--- predict.py
+++ predict.py
@@ -3,16 +3,20 @@ import os
 import shutil
 import tempfile
 import torch
-import joblib
 import sys
 from contextlib import contextmanager
 from torchvision import transforms as T
+from src.data.MultimodalDataset import MultimodalDataset
+import json
+import matplotlib.pyplot as plt
+from PIL import Image
 
 sys.path.append("./src")
 from metadata import generate_metadata
 from Transformation import generate_transformation, save_histogram_as_image
 from preprocessing.loader import Loader
 from model.MiniMobileNet import MiniMobileNet
+
 
 @contextmanager
 def get_tmp_images(image_path: str):
@@ -38,12 +42,18 @@ def get_tmp_images(image_path: str):
     finally:
         shutil.rmtree(root_dir)
 
-def predict(model_path, image_path, encoder_path, device='cuda' if torch.cuda.is_available() else 'cpu'):
+
+def predict(model_path, image_path, label_encoding,
+            device='cuda' if torch.cuda.is_available() else 'cpu'):
     transform = T.Compose([
         T.Resize((224, 224)),
         T.ToTensor(),
-        T.Normalize(mean=[0.37272489070892334, 0.3830896019935608, 0.3935730457305908], 
-                    std=[0.31965553760528564, 0.3071448802947998, 0.3268057703971863])
+        T.Normalize(mean=[0.37272489070892334,
+                          0.3830896019935608,
+                          0.3935730457305908],
+                    std=[0.31965553760528564,
+                         0.3071448802947998,
+                         0.3268057703971863])
         ])
 
     model = MiniMobileNet(csv_dim=1503, n_classes=8)
@@ -51,35 +61,50 @@ def predict(model_path, image_path, encoder_path, device='cuda' if torch.cuda.is
     model.to(device)
     model.eval()
 
-    label_encoder = joblib.load(encoder_path)
-
-    # --- Générer les variantes d’image ---
-
-    # Ici tu peux faire le code qui permet a de Image Path donner en argument cree dans un tmp
-    # les images du transform + csv + image de l'histograme (je dois avoir le csv pour le model mais l'image egalementpour l'afficher comme demande le sujet)
-    # dans le tmp je veux egalement la csv a une ligne avec "class,original,images" mais avec class vide 
+    with open(label_encoding, "r") as f:
+        label_maps = json.load(f)
+        index_to_label = {int(k): v for k,
+                          v in label_maps["index_to_label"].items()}
 
     with get_tmp_images(image_path) as (csv_path, images_path):
-        # le csv_path comme demandé et images_path qui contient les 6 transformations sous forme d'image (avec l'histograme)
-        # le dossier temporaire est automatiquement supprimé quand tu sors du contexte du with (c'est avec contextmanager et yield)
-        print(csv_path)
-        print(images_path)
+        predict_data = MultimodalDataset(csv_path, transform=transform,
+                                         csv_dim=1503,
+                                         labels_json_path=label_encoding)
+        imgs_tensor, hist_tensor, _ = predict_data.__getitem__(0)
+        imgs_tensor = [img.unsqueeze(0).to(device) for img in imgs_tensor]
+        hist_tensor = hist_tensor.unsqueeze(0).to(device)
+        with torch.no_grad():
+            output = model(imgs_tensor, hist_tensor)
+            pred = output.argmax(dim=1).item()
+            predicted_class = index_to_label[pred]
 
-    # --- Prédiction ---
-    # with torch.no_grad():
-    #     output = model(imgs_tensor, hist_tensor)
-    #     pred = output.argmax(dim=1).item()
-
-    # predicted_class = label_encoder.inverse_transform([pred])[0]
-
-    # --- Affichage ---
+        fig, axs = plt.subplots(2, 4, figsize=(16, 8))
+        images_path.append(image_path)
+        for idx, image_path in enumerate(images_path):
+            if idx >= 8:
+                break
+            img = Image.open(image_path).convert("RGB")
+            ax = axs[idx // 4][idx % 4]
+            ax.imshow(img)
+            ax.axis("off")
+            if idx < 6:
+                ax.set_title(f"Transfo {idx+1}")
+            else:
+                ax.set_title("Original Image")
+        axs[-1][-1].axis('off')
+        fig.suptitle(f"Predicted class: {predicted_class}", fontsize=20)
+        plt.tight_layout()
+        plt.show()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model", required=True, help="Path to the trained model (.pt)")
-    parser.add_argument("--image", required=True, help="Path to the input image")
-    parser.add_argument("--encoder", required=True, help="Path to the label_encoder.pkl")
+    parser.add_argument("--model", required=True,
+                        help="Path to the trained model (.pt)")
+    parser.add_argument("--image", required=True,
+                        help="Path to the input image")
+    parser.add_argument("--label_encoding", required=True,
+                        help="Path to the label.json file")
     args = parser.parse_args()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    predict(args.model, args.image, args.encoder, device=device)
+    predict(args.model, args.image, args.label_encoding, device=device)
