b378530408f0a4f5dc8d739de39f5daee2af8ed3
nmilan <milan.nicolas@rocketmail.com>
2025-06-24 02:03:59 +0200

    norm flake8 to code

--- train.py
+++ train.py
@@ -5,30 +5,40 @@ from src.model.MiniMobileNet import MiniMobileNet
 from src.data.MultimodalDataset import MultimodalDataset
 import torchvision.transforms as T
 
+
 def main():
-	print(f"Using device: {'cuda' if torch.cuda.is_available() else 'cpu'}")
-	model = MiniMobileNet(csv_dim=1503, n_classes=8)
-	transform = T.Compose([
-    	T.Resize((224, 224)),
-    	T.ToTensor(),
-    	T.Normalize(mean=[0.37272489070892334, 0.3830896019935608, 0.3935730457305908], 
-                std=[0.31965553760528564, 0.3071448802947998, 0.3268057703971863])
-		])
+    print(f"Using device: {'cuda' if torch.cuda.is_available() else 'cpu'}")
+    model = MiniMobileNet(csv_dim=1503, n_classes=8)
+    transform = T.Compose([
+        T.Resize((224, 224)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.37272489070892334,
+                          0.3830896019935608,
+                          0.3935730457305908],
+                    std=[0.31965553760528564,
+                         0.3071448802947998,
+                         0.3268057703971863])
+        ])
 
-	train_dataset = MultimodalDataset("train.csv", transform=transform, csv_dim=1503)
-	train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=8, pin_memory=True)
-	val_dataset = MultimodalDataset("test.csv", transform=transform, csv_dim=1503)
-	val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=8, pin_memory=True)
+    train_dataset = MultimodalDataset("train.csv",
+                                      transform=transform, csv_dim=1503)
+    train_loader = DataLoader(train_dataset, batch_size=64,
+                              shuffle=True, num_workers=8, pin_memory=True)
+    val_dataset = MultimodalDataset("test.csv",
+                                    transform=transform, csv_dim=1503)
+    val_loader = DataLoader(val_dataset, batch_size=64,
+                            shuffle=False, num_workers=8, pin_memory=True)
 
-	train_model(
-		model=model,
-		train_loader=train_loader,
-		val_loader=val_loader,
-		num_epochs=300,
-		lr=1e-4,
-		device='cuda' if torch.cuda.is_available() else 'cpu',
-		save_path='best_model.pt'
-	)
- 
+    train_model(
+        model=model,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        num_epochs=300,
+        lr=1e-4,
+        device='cuda' if torch.cuda.is_available() else 'cpu',
+        save_path='best_model.pt'
+    )
+
+
 if __name__ == "__main__":
-	main()
+    main()
