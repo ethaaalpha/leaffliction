a06c0f645b37eef2f0647f9313e6e1f645ae3484
Ethaniel Billon <ebillon@student.42lyon.fr>
2025-06-25 14:03:36 +0200

    remove duplicate

--- install_torch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source .venv/bin/activate
-# Install PyTorch (2.7.1 during dev of the project) with CUDA 12.8 support
-pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu128
