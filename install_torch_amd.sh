d44928a61392c3d1553b09db1f386ec3df61a4ea
Ethaniel Billon <ebillon@z3r2p4.42lyon.fr>
2025-06-26 11:33:32 +0200

    pip option for torch

--- install_torch_amd.sh
+++ install_torch_amd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source .venv/bin/activate
 # Install PyTorch (2.7.1 during dev of the project) with ROCM 6.3 support
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.3
+pip3 install torch torchvision torchaudio --force-reinstall --index-url https://download.pytorch.org/whl/rocm6.3
